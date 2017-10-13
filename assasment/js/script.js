427fa333487f28575964eb12ce6a0b1eeb581176
Adrian <aziolko94@gmail.com>
2017-10-13 11:35:53 +0100

    removed assasment

--- assasment/js/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-   $(".container p").hide();
-    $(".container i").click(function(){
-        $(this).parent().next("p").slideToggle();
-        $(this).parent().toggleClass("current");
-    });
-});
